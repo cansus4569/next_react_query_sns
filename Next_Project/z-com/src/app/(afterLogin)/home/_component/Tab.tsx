d504de2fbb182414329534149c7507f97f0a9581
cansus4569 <pjs25563@gmail.com>
2024-06-03 21:46:53 +0900

    - 홈탭 만들기 Context API 적용

---
+++ Next_Project/z-com/src/app/(afterLogin)/home/_component/Tab.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import style from './tab.module.css';
+
+const Tab = () => {
+  const [tab, setTab] = React.useState('rec');
+
+  const onClickRec = () => {
+    setTab('rec');
+  };
+
+  const onClickFol = () => {
+    setTab('fol');
+  };
+
+  return (
+    <div className={style.homeFixed}>
+      <div className={style.homeText}>홈</div>
+      <div className={style.homeTab}>
+        <div onClick={onClickRec}>
+          추천
+          <div className={style.tabIndicator} hidden={tab === 'fol'}></div>
+        </div>
+        <div onClick={onClickFol}>
+          팔로우 중
+          <div className={style.tabIndicator} hidden={tab === 'rec'}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tab;
